87cb7b4cffd8e62f84fcd9c59f2ac9b25b3e8e1c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-05-27 18:56:00 +0300

    Create a new folder.

---
+++ Homework/Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num1 = double.Parse(Console.ReadLine());
+        double num2 = double.Parse(Console.ReadLine());
+        double result = (FactorialFirst(num1, num2) / (FactorialSecond(num1, num2)));
+        Console.WriteLine($"{result:f2}");
+    }
+    static double FactorialFirst (double num1, double num2)
+    {
+        double result1 = 1;
+        for ( int i = 1; i <= num1; i++ )
+        {
+            result1 *= i; 
+        }
+        return result1;
+    }
+    static double FactorialSecond (double num1, double num2)
+    {
+        double result2 = 1; 
+        for (int i = 1; i <= num2; i++)
+        {
+            result2 *= i; 
+        }
+        return result2; 
+    }
+}
