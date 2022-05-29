87971f1260bd911f8583fc1ffe8cbdb75ee1e9a6
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-05-29 17:00:02 +0300

    Added new folder

---
+++ Homework/Fundamentals/FinalExamPreparation/02 .Fancy Barcodes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SecretChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                string number = string.Empty;
+                Match match = Regex.Match(name, @"@{1}#+(?<product>[A-Z*][A-Za-z\d]{4,}[A-Z])@{1}#+");
+                string nameProduct = match.Groups["product"].Value;
+                if (nameProduct.Length == 0)
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+                else
+                {
+
+                    for (int j = 0; j < nameProduct.Length; j++)
+                    {
+                        if (char.IsDigit(nameProduct[j]))
+                        {
+                            number += nameProduct[j];
+                        }
+                    }
+                    if (number.Length > 0)
+                    {
+                        Console.WriteLine($"Product group: {number}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Homework/Fundamentals/FinalExamPreparation/04. MirrorWords/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Pirates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, @"(@|#)([A-Za-z]{3,})(\1)(\1)([A-Za-z]{3,})(\1)");
+            List <string> output = new List<string>();  
+
+            foreach (Match match in matches)
+            {
+                string firstWord = match.Groups[2].Value; 
+                string secondWord = match.Groups[5].Value;
+
+                char[] newWord = secondWord.ToCharArray();
+                Array.Reverse(newWord);
+                string reversedWord = new string(newWord); 
+ 
+                if(firstWord == reversedWord)
+                {
+                    output.Add($"{firstWord} <=> {secondWord}");
+                }
+             }
+            if(matches.Count == 0)
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+            else if(matches.Count > 0)
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+            }
+            if(output.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else if(output.Count > 0)
+            {
+                Console.WriteLine("The mirror words are:");
+                    Console.Write(string.Join(", ",output));
+            }
+        }
+    }
+}
