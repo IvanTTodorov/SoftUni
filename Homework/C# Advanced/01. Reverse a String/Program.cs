af6034a302922f6c6c97ae9261e3229987ad8a4f
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-01 20:36:32 +0300

    Added a task

---
+++ Homework/C# Advanced/01. Reverse a String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string input = Console.ReadLine();   
+            Stack<char> stack = new Stack<char>();
+            foreach (var item in input) 
+            {
+                stack.Push(item);
+            } 
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop()); 
+            }
+        }
+    }
+}
