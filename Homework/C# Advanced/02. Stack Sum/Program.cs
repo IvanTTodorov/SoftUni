e76eb8769b5a1e1e5ecc93211e02c2be2c40e69a
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-01 20:37:05 +0300

    Added a task

---
+++ Homework/C# Advanced/02. Stack Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray(); 
+        Stack<int> stack = new Stack<int>(numbers);
+
+       
+        string command = String.Empty;
+        while ((command = Console.ReadLine().ToLower()) != "end")
+        {  
+            string[] cmdItems = command.Split();
+            string commandText = cmdItems[0];
+            
+             
+            if (commandText == "add")
+            {
+                stack.Push(int.Parse(cmdItems[1]));
+                stack.Push(int.Parse(cmdItems[2])); 
+            } 
+            else
+            {
+                int index = int.Parse(cmdItems[1]); 
+                if (index < stack.Count)
+                {
+                    for (int i = 0; i < index; i++)
+                    {
+                        stack.Pop();
+                    }
+                } 
+                
+            }
+        }
+        Console.WriteLine($"Sum: {stack.Sum()}");
+    }
+}
