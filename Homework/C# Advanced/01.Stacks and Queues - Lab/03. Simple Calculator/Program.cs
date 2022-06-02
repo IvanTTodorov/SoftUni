1e57958dc5c8d6cbff5042efd05c0841a5a14190
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-02 21:21:05 +0300

    Added a new task

---
+++ Homework/C# Advanced/01.Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] numberOperations = Console.ReadLine().Split();
+        Stack<string> stack = new Stack<string>();
+        Stack<string> stackReverse = new Stack<string>();      
+
+        int result = 0;
+
+        for (int i = 0; i < numberOperations.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                stack.Push(numberOperations[i]);
+            }
+        }
+
+        while (stack.Count > 0)
+        {
+           stackReverse.Push(stack.Pop());
+        }
+
+        for (int i = 0; i < numberOperations.Length; i++)
+        { 
+            if ( i == 0)
+            {
+                result += int.Parse(stackReverse.Pop());
+            }
+            if (numberOperations[i] == "+")
+            {
+                result += int.Parse(stackReverse.Pop());
+            } 
+            else if (numberOperations[i] == "-")
+            {
+                result -= int.Parse(stackReverse.Pop());
+            }
+        }
+
+        Console.WriteLine(result);
+
+      
+    }
+}
+        
+
