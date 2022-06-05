12f22663f93602cb40e8be009ed63476a588c633
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-05 20:50:34 +0300

    Added a new task

---
+++ Homework/C# Advanced/01.Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> cars = new Queue<string>();
+        int n = int.Parse(Console.ReadLine());
+        string command = string.Empty;
+        int totalCars = 0;
+
+        while ((command = Console.ReadLine()) != "end")
+        {
+            if (command != "green")
+            {
+                cars.Enqueue(command);
+            }
+            else if (command == "green")
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (cars.Count > 0)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        totalCars++;
+                    }
+                }
+
+            }
+        }
+        Console.WriteLine($"{totalCars} cars passed the crossroads.");
+    }
+}
