3411d9747bc71b9dc318173335b6140c80f14523
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-06 18:03:35 +0300

    Added a new task

---
+++ Homework/C# Advanced/01.Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int foodSum = int.Parse(Console.ReadLine());
+        int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Queue<int> numbers = new Queue<int>(orders);
+        Console.WriteLine(numbers.Max()); 
+
+        for (int i = 0; i < orders.Length; i++)
+        {
+            if (foodSum >= numbers.Peek())
+            {
+                foodSum -= numbers.Peek();
+                numbers.Dequeue();
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {String.Join(" ", numbers)}");
+                break;
+            }
+        }
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("Orders complete");
+        }
+        
+    }
+}
