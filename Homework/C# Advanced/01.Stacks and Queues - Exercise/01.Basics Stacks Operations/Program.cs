b3538b4f0020969b7f51f660e83334298dcc649c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-06 18:01:38 +0300

    Added a new folder

---
+++ Homework/C# Advanced/01.Stacks and Queues - Exercise/01.Basics Stacks Operations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int n = input[0];
+        int s = input[1];
+
+        int valid = input[2];
+
+        Stack<int> nums = new Stack<int>();
+        for (int i = 0; i < n; i++)
+        {
+            nums.Push(numbers[i]);
+        }
+        for (int i = 0; i < s; i++)
+        {
+            nums.Pop();
+        }
+        if (nums.Contains(valid))
+        {
+            Console.WriteLine("true");
+        }
+        else if (!nums.Contains(valid))
+        {
+            if (nums.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(nums.Min());
+            }
+        }
+    }
+}
+
