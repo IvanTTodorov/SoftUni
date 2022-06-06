62f3e00cf372d1cd1861d5247dd8a80f40b1db3c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-06 18:03:06 +0300

    Added a new task

---
+++ Homework/C# Advanced/01.Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Stack<int> numbers = new Stack<int>();  
+        for (int i = 0; i < n; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (input[0] == 1)
+            {
+                numbers.Push(input[1]); 
+            }
+            else if (input[0] == 2)
+            {
+                numbers.Pop();
+            }
+            else if(input[0] == 3 && numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Max());
+            } else if (input[0] == 4 && numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+        Console.WriteLine(String.Join(", ", numbers));
+     
+    }
+}
