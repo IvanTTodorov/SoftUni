aa5f3d39fca3017e6524ef9440786a610fa9c34c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-07 21:55:46 +0300

    Added a new task

---
+++ Homework/C# Advanced/01.Stacks and Queues - Exercise/08. Balanced Parentheses/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Stack<char> firstBrackets = new Stack<char>();
+
+
+
+        foreach (var bracket in input)
+        {
+            if (bracket == '(' || bracket == '{' || bracket == '[')
+            {
+                firstBrackets.Push(bracket);
+            }
+
+            if (firstBrackets.Count == 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            else if (bracket == ')' || bracket == '}' || bracket == ']')
+            {
+                char symbol = firstBrackets.Pop();
+                if ((symbol == '(' && bracket == ')')
+                    || (symbol == '{' && bracket == '}')
+                    || (symbol == '[' && bracket == ']'))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+
+
+            }
+        }
+        Console.WriteLine("YES");
+    }
+}
