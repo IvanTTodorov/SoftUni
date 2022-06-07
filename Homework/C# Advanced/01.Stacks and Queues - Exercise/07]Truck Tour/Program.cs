5ef4def99aec265ae5d2d6dee2f0c06352174c65
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-07 21:55:05 +0300

    Added a new task

---
+++ Homework/C# Advanced/01.Stacks and Queues - Exercise/07]Truck Tour/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+        Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+        int value = 0;
+        int diff = 0;
+
+        for (int i = 0; i <= n - 1; i++)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            dict.Add(i, new List<int>());
+            dict[i].Add(numbers[0]);
+            dict[i].Add(numbers[1]);
+
+        }
+        while (true)
+            foreach (var items in dict)
+            {
+                diff += items.Value[0];
+                diff -= items.Value[1];
+                 
+                if (diff >= 0)
+                {
+                    count++;
+                    if (count == 1)
+                    {
+                        value = items.Key;
+
+                    }
+                    if (count > n)
+                    {
+                        Console.WriteLine(value);
+                        return;
+                    }
+                }
+                else
+                {
+                    diff = 0;
+                    count = 0;
+                }
+            }
+    }
+}
+
+
