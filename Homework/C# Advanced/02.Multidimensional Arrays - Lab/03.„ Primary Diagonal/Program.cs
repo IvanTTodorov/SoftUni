640b7976f8150034d686fb40b7acd0f004933df0
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-08 20:20:12 +0300

    Added a new task.

---
+++ Homework/C# Advanced/02.Multidimensional Arrays - Lab/03.„ Primary Diagonal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] number = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            for (int j = 0; j < n; j++)
+            {
+
+                matrix[i, j] = number[j];
+            }
+        }
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, i]; 
+        }
+        Console.WriteLine(sum);
+    }
+} 
