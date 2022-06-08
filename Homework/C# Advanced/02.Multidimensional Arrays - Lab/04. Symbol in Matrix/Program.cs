b46e5fe1b6b4b1f198a0270acff50abe37cddeb8
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-08 20:21:32 +0300

    Added a new task

---
+++ Homework/C# Advanced/02.Multidimensional Arrays - Lab/04. Symbol in Matrix/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        char[,] matrix = new char[number, number];
+
+        for (int i = 0; i < number; i++)
+        {
+            string text = Console.ReadLine();
+            for (int j = 0; j < number; j++)
+            {
+                matrix[i, j] = text[j];
+            }
+        }
+        char symbol = char.Parse(Console.ReadLine());
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == symbol)
+                {
+                    Console.WriteLine($"({i}, {j})");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine($"{symbol} does not occur in the matrix");
+    }
+}
+
+
