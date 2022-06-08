1b6bbedeb6570cc3b05bb887a891a158bce55f8c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-08 20:19:21 +0300

    Added a new task

---
+++ Homework/C# Advanced/02.Multidimensional Arrays - Lab/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        int first = nums[0];
+        int second = nums[1];
+        int[,] matrix = new int[first, second];
+        int sum = 0;
+
+        for (int i = 0; i < first; i++)
+        {
+            nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            for (int j = 0; j < second; j++)
+            {
+
+                matrix[i, j] = nums[j];
+            }
+        }
+
+        for (int i = 0; i < second; i++)
+        {
+            for (int j = 0; j < first; j++)
+            {
+                sum += matrix[j, i];
+            }
+            Console.WriteLine(sum);
+            sum = 0;
+        }
+    }
+}
