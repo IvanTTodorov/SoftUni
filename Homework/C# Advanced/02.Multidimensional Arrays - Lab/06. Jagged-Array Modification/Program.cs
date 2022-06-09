99fb7c3fd015e579474d2732c22ddb16ea9da1ae
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-09 18:56:01 +0300

    Added a new task

---
+++ Homework/C# Advanced/02.Multidimensional Arrays - Lab/06. Jagged-Array Modification/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rowNum = int.Parse(Console.ReadLine());
+        int[][] matrix = new int[rowNum][]; 
+
+        for (int row = 0; row < matrix.Length; row++)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            matrix[row] = new int[arr.Length]; 
+
+            for (int col = 0; col < arr.Length; col++)
+            {
+                matrix[row][col] = arr[col]; 
+            }
+        }
+        string[] command = Console.ReadLine().Split().ToArray();
+        while (command[0] != "END")
+        {
+
+            string text = command[0];
+            int row = int.Parse(command[1]);
+            int col = int.Parse(command[2]);
+            int num = int.Parse(command[3]);
+
+            if (matrix.Length > row && row >= 0 && col >= 0 && matrix[row].Length > col)
+            {
+                if (text == "Add")
+                {
+                    matrix[row][col] += num;
+                }
+                else if (text == "Subtract")
+                {
+                    matrix[row][col] -= num;
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Invalid coordinates");
+            }
+            command = Console.ReadLine().Split().ToArray();
+
+
+        }
+
+        foreach (var item in matrix)
+        {
+            Console.WriteLine(String.Join(" ", item));
+        }
+    }
+}
