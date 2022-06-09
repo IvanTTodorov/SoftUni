648a7bd2e97ff3f9525b8b3b9b6e0f2f5c3b18cf
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-09 18:59:59 +0300

    Added a new task

---
+++ Homework/Programming Basics with Java/For Loop/Lab/CharacterSequence.java
@@ -0,0 +1,13 @@
+package Lecture;
+
+import java.util.Scanner;
+
+public class CharacterSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        for (int i = 0; i <= text.length()-1; i++) {
+            System.out.println(text.charAt(i)); }
+    }
+}
