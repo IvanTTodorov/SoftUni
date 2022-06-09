a1c0eefa2b598785c27bd8ceafe948a9c06b3415
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-09 19:00:29 +0300

    Added a new task

---
+++ Homework/Programming Basics with Java/For Loop/Lab/EvenPowers.java
@@ -0,0 +1,14 @@
+package Lecture;
+
+import java.util.Scanner;
+
+public class EvenPowers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i <= n; i += 2) {
+            double result = Math.pow(2,i);
+            System.out.printf("%.0f%n", result);
+        }
+    }
+}
