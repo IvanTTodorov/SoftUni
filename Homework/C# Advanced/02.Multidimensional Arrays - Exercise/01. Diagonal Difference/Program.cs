aaeb236aca9863aa8d827b47468fc4ae9e786375
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-10 21:25:54 +0300

    Added a new task.

---
+++ Homework/C# Advanced/02.Multidimensional Arrays - Exercise/01. Diagonal Difference/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[n, n];
+        int sum = 0;
+        int sum1 = 0; 
+        
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                matrix[row, col] = numbers[col]; 
+            }
+        }
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, i];
+            sum1 += matrix[i, matrix.GetLength(1) - i - 1]; 
+        }
+        Console.WriteLine(Math.Abs(sum - sum1));
+    }
+}
