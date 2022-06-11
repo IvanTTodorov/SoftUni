1951026afe8bc66b634565f8f5b618c453f65608
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-11 20:36:59 +0300

    Added a new task

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] arr = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        Dictionary<double, int> dict = new Dictionary<double, int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!dict.ContainsKey(arr[i]))
+            {
+                dict.Add(arr[i], 1);
+            }
+            else
+            {
+                dict[arr[i]] += 1;
+            }
+        }
+
+        foreach (var item in dict)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value} times");
+        }
+
+    }
+}
