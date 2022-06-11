d277eca0d29dcd4d2943c5d250735f08e0650404
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-11 20:39:28 +0300

    Added a new task

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, Dictionary<string, List<string>>> continent = new Dictionary<string, Dictionary<string, List<string>>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+            string continentName = input[0];
+            string countryName = input[1];
+            string cityName = input[2];
+
+            if (!continent.ContainsKey(continentName))
+            {
+                continent.Add(continentName, new Dictionary<string, List<string>>());
+                continent[continentName].Add(countryName, new List<string>());
+                continent[continentName][countryName].Add(cityName);
+            }
+            else
+            {
+                if (!continent[continentName].ContainsKey(countryName))
+                {
+                    continent[continentName].Add(countryName, new List<string>());
+                    continent[continentName][countryName].Add(cityName);
+                }
+                else
+                {
+                    if (!continent[continentName][countryName].Contains(cityName))
+                    {
+                        continent[continentName][countryName].Add(cityName);
+                    }
+                }
+            }
+        }
+        foreach (var item in continent)
+        {
+            Console.WriteLine($"{item.Key}:");
+            foreach (var country in item.Value)
+            {
+                Console.WriteLine($"{country.Key} -> {String.Join(", ", country.Value)}");
+            }
+
+        }
+    }
+}
