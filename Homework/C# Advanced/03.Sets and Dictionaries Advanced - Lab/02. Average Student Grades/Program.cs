5f3684bbbf1c9e3b4b80ad78e00d4265ce0b5110
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-11 20:37:34 +0300

    Added a new task.

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+       
+        for (int i = 0; i < n; i++)
+        {
+           string[] input = Console.ReadLine().Split();
+            string name = input[0];
+            decimal mark = decimal.Parse(input[1]); 
+
+            if(!students.ContainsKey(name))
+            {
+                students.Add(name, new List<decimal>());
+                students[name].Add(mark);
+            }
+            else
+            {
+                students[name].Add(mark);
+            }
+        }
+        foreach (var item in students)
+        {
+            StringBuilder allgrades = new StringBuilder();
+            for (int i = 0; i < item.Value.Count; i++)
+            {
+                allgrades.Append($"{item.Value[i]:f2} ");
+            }
+            Console.WriteLine($"{ item.Key} -> {allgrades.ToString()}(avg: {item.Value.Average():f2})");
+        }
+    }
+}
