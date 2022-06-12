47f15660c0344668558ee4d591c372862fbde04d
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-12 21:12:26 +0300

    Added a new task

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        HashSet<string> names = new HashSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            string name = Console.ReadLine();
+            names.Add(name);
+        }
+        foreach(var item in names)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
