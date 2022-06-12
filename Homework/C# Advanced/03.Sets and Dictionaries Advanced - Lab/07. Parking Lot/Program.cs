52b04f03c9c4f57d2e56243dd21e0162951b2c24
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-12 21:13:12 +0300

    Added a new task.

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string command = Console.ReadLine();
+        HashSet<string> carsInParking = new HashSet<string>();
+
+        while (command != "END")
+        {
+            string[] cars = command.Split(", ");
+            string inOrOut = cars[0];
+            string numbersCars = cars[1];
+
+
+            if (inOrOut == "IN")
+            {
+                carsInParking.Add(numbersCars);
+            }
+            else if (inOrOut == "OUT")
+            {
+                carsInParking.Remove(numbersCars);
+            }
+            command = Console.ReadLine();
+        }
+        if (carsInParking.Count > 0)
+        {
+           foreach (var item in carsInParking)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
