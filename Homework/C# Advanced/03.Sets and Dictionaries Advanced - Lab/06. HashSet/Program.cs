4715f7115c6f940b5984a47a44d38b70fffb2a22
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-12 21:11:49 +0300

    Added a new task.

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Lab/06. HashSet/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int counter = 1000000;
+        int containsCounter = 100000; 
+
+        List<string> list = new List<string>(counter);
+        HashSet<string> hash = new HashSet<string>(counter);
+        StringHashSet ourSet = new StringHashSet(counter);
+
+        for (int i = 0; i < counter; i++)
+        {
+            list.Add(i.ToString());
+            hash.Add(i.ToString());
+            ourSet.Add(i.ToString());
+        }
+        Console.WriteLine("Start:");
+        var timer = Stopwatch.StartNew();
+
+        /* for (int i = 0; i < containsCounter; i++)
+        {
+            bool exist = list.Contains(i.ToString()); 
+        }
+        
+        timer.Stop();
+        Console.WriteLine("List:");
+        Console.WriteLine(timer.ElapsedMilliseconds);
+        timer.Reset();
+        timer.Start(); */
+
+        for (int i = 0; i < containsCounter; i++)
+        {
+            bool exist = hash.Contains(i.ToString());
+        }
+
+        timer.Stop();
+        Console.WriteLine("Hash of C#:");
+        Console.WriteLine(timer.ElapsedMilliseconds);
+        timer.Reset();
+        timer.Start();
+
+        for (int i = 0; i < containsCounter; i++)
+        {
+            bool exist = ourSet.Contains(i.ToString());
+        }
+
+        timer.Stop();
+        Console.WriteLine("Ours:");
+        Console.WriteLine(timer.ElapsedMilliseconds);
+        timer.Reset();
+        timer.Start();
+    }
+    static string GenerateString(int length=8)
+    {
+
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++)
+        {
+            sb.Append(random.Next(0, 9)); 
+        }
+        return sb.ToString();
+    }
+}
+
+public class StringHashSet
+{
+    private SetElement[] array;
+    public StringHashSet(int capacity = 8)
+    {
+        array = new SetElement[capacity];
+    }
+    public void Add(string key)
+    {
+        int index = HashFunction(key);
+        if (array[index] != null)
+        {
+            array[index].Keys.Add(key);
+        }
+        else
+        {
+            array[index] = new SetElement() { Keys = new List<string>() { key } };
+        }
+
+    }
+    private int HashFunction(string key)
+    {
+        return Math.Abs(key.GetHashCode() % array.Length);
+        /* int asciiResult = key[0] << 5 | key[key.Length - 1] >> 4;
+        return asciiResult % array.Length;
+        */
+    }
+    public bool Contains(string key)
+    {
+        int index = HashFunction(key);
+        if (array[index] != null)
+        {
+            for (int i = 0; i < array[index].Keys.Count; i++)
+            {
+                if (array[index].Keys[i] == key)
+                {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+}
+public class SetElement
+{
+    public List<string> Keys { get; set; }
+}
---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Lab/06. HashSet/StringHashSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99._HashSet
+{
+   public class StringHashSet
+    {
+        private string[] array; 
+        public StringHashSet(int capacity = 8)
+        {
+            array = new string[capacity];
+        }
+        public void Add (string key)
+        {
+            array[HashFunction(key)] = key;
+        }
+        private int HashFunction (string key)
+        {
+            int asciiResult = key[0] << 5 & key[key.Length - 1] >> 4;
+            return asciiResult % array.Length;
+        }
+        public bool Contains (string key)
+        {
+            if(array[HashFunction(key)] == key)
+            {
+                return true; 
+            }
+            return false; 
+        }
+    }
+}
