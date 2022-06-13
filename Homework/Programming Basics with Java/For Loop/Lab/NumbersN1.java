2bf1601aebdb1834a44cf3599dd54852ea1fb1dd
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-13 22:02:18 +0300

    Added a new task

---
+++ Homework/Programming Basics with Java/For Loop/Lab/NumbersN1.java
@@ -0,0 +1,15 @@
+package Lecture;
+
+import java.util.Scanner;
+
+public class NumbersN1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 100; i >= 10; i-=10)  {
+            System.out.println(i);
+        }
+
+    }
+}
