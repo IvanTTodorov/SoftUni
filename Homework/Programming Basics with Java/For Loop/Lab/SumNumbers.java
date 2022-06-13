566b329b5d12c86f3a0687ed48d1031f88fbc303
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-13 22:04:21 +0300

    Added a new task

---
+++ Homework/Programming Basics with Java/For Loop/Lab/SumNumbers.java
@@ -0,0 +1,19 @@
+package Lecture;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            sum += number;
+
+        }
+        System.out.println(sum);
+    }
+}
