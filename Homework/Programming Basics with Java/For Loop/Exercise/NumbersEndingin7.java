6b82f41b7b0af918177c37465ac954564a2abe5e
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-14 21:59:28 +0300

    Added a new task

---
+++ Homework/Programming Basics with Java/For Loop/Exercise/NumbersEndingin7.java
@@ -0,0 +1,13 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class NumbersEndingin7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 7; i <= 1000; i+=10  ) {
+            System.out.println(i);
+        }
+    }
+}
