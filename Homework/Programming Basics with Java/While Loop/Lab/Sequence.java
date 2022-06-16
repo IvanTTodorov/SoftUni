3b24751300641e51db6e43a907a622e4d72c45bb
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-16 21:46:09 +0300

    Added a new task

---
+++ Homework/Programming Basics with Java/While Loop/Lab/Sequence.java
@@ -0,0 +1,22 @@
+package Lecture;
+
+import java.util.Scanner;
+
+public class Sequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    int n = Integer.parseInt (scanner.nextLine());
+int i = 1;
+    while (n >= i) {
+        System.out.println(i);
+i = i *2 + 1;
+
+       }
+
+
+
+
+
+    }
+}
