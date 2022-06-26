601afbbd4e94dcb5e582bb00f5db7cba9281f35b
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-06-26 21:14:02 +0300

    Added a new task

---
+++ Homework/Programming Basics with Java/Nested Loop/More Exercise/CarNumber.java
@@ -0,0 +1,37 @@
+package MoreExercises;
+
+import java.util.Scanner;
+
+public class CarNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int start = Integer.parseInt(scanner.nextLine());
+        int end = Integer.parseInt(scanner.nextLine());
+
+        for (int i = start; i <= end; i++) {
+            for (int j = start; j <= end; j++) {
+                for (int k = start; k <= end; k++) {
+                    for (int l = start; l <= end; l++) {
+
+                         if ((i % 2 == 0) && (l % 2 != 0)) {
+                            if (i > l) {
+                                if ((j + k) % 2 == 0) {
+                                    System.out.printf("%d%d%d%d ", i, j, k, l);
+                                }
+                            }
+                        } else if ((i % 2 != 0) && (l % 2 == 0)) {
+                            if (i > l) {
+                                if ((j + k) % 2 == 0) {
+                                    System.out.printf("%d%d%d%d ", i, j, k, l);
+
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
