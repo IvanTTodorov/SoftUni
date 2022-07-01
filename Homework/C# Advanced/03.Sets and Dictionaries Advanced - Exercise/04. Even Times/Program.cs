159c0225e661f7f520bf0ef78140a9762345b5f5
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-01 21:30:17 +0300

    Added a new task

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<int, int> numbersDict = new Dictionary<int, int>();
+        
+ 
+        for (int i = 0; i < n; i++)
+        {
+            int numbers = int.Parse(Console.ReadLine()); 
+           
+            if(!numbersDict.ContainsKey(numbers))
+                {
+                    numbersDict.Add(numbers, 1);
+                } 
+                else
+                {
+                    numbersDict[numbers]++; 
+                }                       
+            } 
+          
+        foreach (var i in numbersDict)
+        {
+            if (i.Value % 2 == 0)
+            {
+                Console.WriteLine(i.Key);
+            }
+        }
+    }
+}
