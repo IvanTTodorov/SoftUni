dde4ffbb977c22dd09c20194c3eb6a19d55375ff
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-01 21:30:54 +0300

    Added a new task

---
+++ Homework/C# Advanced/03.Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine(); 
+        Dictionary<char, int> newDict = new Dictionary<char, int>();
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            if (!newDict.ContainsKey(text[i]))
+            {
+                newDict.Add(text[i], 1);
+            }
+            else
+            {
+                newDict[text[i]]++;
+            }
+        }
+        foreach (var item in newDict.OrderBy(x => x.Key))
+        {
+            Console.WriteLine($"{item.Key}: {item.Value} time/s");
+        }
+    }
+}
