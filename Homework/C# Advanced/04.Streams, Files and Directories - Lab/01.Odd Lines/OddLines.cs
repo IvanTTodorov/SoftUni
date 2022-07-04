89a1efc3594f8811e9ab93e744c31436655a7898
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-04 22:33:24 +0300

    Added a new task

---
+++ Homework/C# Advanced/04.Streams, Files and Directories - Lab/01.Odd Lines/OddLines.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.IO; 
+
+namespace OddLines
+{
+    using System;
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int line = 0;
+                    string lineString;
+                    while((lineString = reader.ReadLine()) != null)
+                    {
+                        if (line % 2 == 1)
+                        {
+                            writer.WriteLine(lineString);
+                            
+                        }
+                        line++;
+                    }
+                    
+                }
+            }
+        }
+    }
+}
