b381af39db79ca981b577f9b49b3e3bd03c4c3ec
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-04 22:34:05 +0300

    Added a new task

---
+++ Homework/C# Advanced/04.Streams, Files and Directories - Lab/02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+                var reader = new StreamReader(inputFilePath);
+                using (reader)
+                {
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                    using (writer)
+                    {
+                        int lineNum = 0;
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            writer.WriteLine(lineNum + " " + line);
+                            lineNum++;
+                        }
+
+                    }
+                }
+        }
+    }
+}
