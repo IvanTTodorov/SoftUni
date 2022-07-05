c4d65045c6b8d82bd931acad4446e223ef3d82b8
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-05 19:17:55 +0300

    Added a new task

---
+++ Homework/C# Advanced/04.Streams, Files and Directories - Lab/05.ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExtractBytes
+{
+    class ExtractSpecialBytes
+    {
+        static void Main(string[] args)
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            StreamReader streamReader = new StreamReader(bytesFilePath);
+            using (streamReader)
+            {
+                StreamReader streamReader1 = new StreamReader(binaryFilePath);
+                using (streamReader1)
+                {
+                    StreamWriter file = new StreamWriter(outputPath);
+                    using (file)
+                    {
+                        byte[] fileBytes = File.ReadAllBytes(binaryFilePath);
+                        var bytesList = new List<String>();
+                        var sb = new StringBuilder();
+
+                        while (!streamReader.EndOfStream)
+                        {
+                            bytesList.Add(streamReader.ReadLine());
+                        }
+                        foreach (var item in fileBytes)
+                        {
+                            if (bytesList.Contains(item.ToString()))
+                            {
+                                sb.AppendLine(item.ToString());
+                            }
+                        }
+                        file.WriteLine(sb.ToString().Trim());
+                    }
+                }
+            }
+        }
+    }
+}
