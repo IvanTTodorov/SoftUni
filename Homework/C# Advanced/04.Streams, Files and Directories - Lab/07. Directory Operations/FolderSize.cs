4ddfe682e0846aea7355eb3ec253f208e7c77380
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-06 19:59:59 +0300

    Added a new task

---
+++ Homework/C# Advanced/04.Streams, Files and Directories - Lab/07. Directory Operations/FolderSize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"C:\Users\Ivan\Desktop\SoftUni";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            decimal folderSize = 0;
+            DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+
+            foreach (var folder in dirInfo.EnumerateFiles("*.*", SearchOption.AllDirectories))
+            {
+                folderSize += folder.Length;
+            }
+            
+
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            using (writer)
+            {
+                writer.Write(folderSize / 1024 + " KB"); 
+            }
+        }
+    }
+}
