61be6c2cd18ca738a7b9b8e26383fbea28730ff7
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-06 20:01:31 +0300

    Added a new task.

---
+++ Homework/C# Advanced/04.Streams, Files and Directories - Exercises/02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,36 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int count = 0;
+            List<string> outputLines = new List<string>();
+
+            foreach (var line in lines)
+            {
+                count++;
+                int countLetters = line.Count(char.IsLetter);
+                int countSymbols = line.Count(char.IsPunctuation);
+
+                string newLine = $"Line {count}: {line} ({countLetters})({countSymbols})";
+                outputLines.Add(newLine);
+            }
+            File.WriteAllLines(outputFilePath, outputLines);
+        }
+    }
+}
+
