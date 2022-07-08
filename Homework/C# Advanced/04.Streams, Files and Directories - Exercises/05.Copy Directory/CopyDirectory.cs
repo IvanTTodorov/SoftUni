93d5aaa425bb32a56cbd256d5c05b7dcbf4a163f
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-08 19:25:55 +0300

    Added a new task.

---
+++ Homework/C# Advanced/04.Streams, Files and Directories - Exercises/05.Copy Directory/CopyDirectory.cs
@@ -0,0 +1,33 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        { 
+            if(Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+           Directory.CreateDirectory(outputPath);
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileName(file); 
+                var copyDestination = Path.Combine(outputPath, fileName);
+                File.Copy(fileName, copyDestination);
+            }
+        }
+    }
+}
