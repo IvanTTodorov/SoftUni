011a7f897c76c99afca5a5b482490c38cabe94ca
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-09 22:36:15 +0300

    Added a new task

---
+++ Homework/C# Advanced/05 Functional Programming - Lab/02. SumNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] elements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+        int count = elements.Count();
+        int sum = elements.Sum();
+        Console.WriteLine(count);
+        Console.WriteLine(sum);
+    }
+}
