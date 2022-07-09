b876c0692170ac3ad6a60081e67233f6f8ec069b
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-09 22:32:40 +0300

    Added a new task.

---
+++ Homework/C# Advanced/05 Functional Programming - Lab/01. SortEvenNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x);
+        Console.WriteLine(String.Join(", ", input));
+    }
+}
