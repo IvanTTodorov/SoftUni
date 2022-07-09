6bd129b8bff02739603ae0f422938349509aa055
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-09 22:30:31 +0300

    Added a new task.

---
+++ Homework/C# Advanced/04.Streams, Files and Directories - Exercises/07.ZipAndExtract/ZipAndExtract.cs
@@ -0,0 +1,38 @@
+﻿namespace ZipAndExtract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class ZipAndExtract
+    {
+        static void Main()
+        {
+            string inputFile = @"..\..\..\NewFolder\copyMe.png";
+            string zipArchiveFile = @"..\..\..\NewFolder\archive.zip";
+            string extractedFile = @"..\..\..\NewFolder\extracted.png";
+
+            ZipFileToArchive(inputFile, zipArchiveFile);
+
+            var fileNameOnly = Path.GetFileName(inputFile);
+            ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+        }
+
+        public static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+        {
+            using (var archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create))
+            {
+                string fileName = Path.GetFileName(inputFilePath);
+                archive.CreateEntryFromFile(inputFilePath, fileName);
+            }
+        }
+
+        public static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+        {
+            var archive = ZipFile.OpenRead(zipArchiveFilePath);
+            var fileForExtraction = archive.GetEntry(fileName);
+
+            fileForExtraction.ExtractToFile(outputFilePath);
+        }
+    }
+}
