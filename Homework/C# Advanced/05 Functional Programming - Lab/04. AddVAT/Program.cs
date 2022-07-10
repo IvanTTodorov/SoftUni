2d3f4f42b61064f984f6a668cf684e2817528c73
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-10 21:50:47 +0300

    Added a new task.

---
+++ Homework/C# Advanced/05 Functional Programming - Lab/04. AddVAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var elements = Console.ReadLine().Split(", ").Select(double.Parse).ToList(); 
+        
+        foreach (var element in elements)
+        {
+            double item = element; 
+            item += element * 0.2;
+            Console.WriteLine($"{item:f2}");
+        }
+       
+    }
+}
