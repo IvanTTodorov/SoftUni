0c950091b36c6966551f12c13a8c0eba0d8f2962
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-10 21:49:58 +0300

    Added a new task

---
+++ Homework/C# Advanced/05 Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+        Func<string,bool> predicate = (word => char.IsUpper(word[0]));
+        List<string> words = input.Where(predicate).ToList();
+        foreach (var word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
