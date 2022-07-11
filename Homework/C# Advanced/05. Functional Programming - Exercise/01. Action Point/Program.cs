a6ec92f4532cdcdd2ca466224e6143a035dcc56e
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-11 21:34:16 +0300

    Added a new task.

---
+++ Homework/C# Advanced/05. Functional Programming - Exercise/01. Action Point/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = Console.ReadLine().Split();
+        Action<string[]> printNames = names => Console.WriteLine(String.Join(Environment.NewLine, names));
+        printNames(names);  
+    }
+}
