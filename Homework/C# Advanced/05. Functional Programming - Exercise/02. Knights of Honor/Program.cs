dbadf481278d26b91c55391c58c9a82f1d7e157c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-11 21:34:45 +0300

    Added a new task

---
+++ Homework/C# Advanced/05. Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> names = Console.ReadLine().Split().ToList();
+        Action<List<string>> printNames = x =>x.ForEach(x => Console.WriteLine($"Sir {x}"));
+        printNames(names);
+    }
+}
