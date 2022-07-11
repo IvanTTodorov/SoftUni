6dd1aa2a306c4e7c813d9a168dd2e9d607bb0180
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-11 21:35:10 +0300

    Added a new task.

---
+++ Homework/C# Advanced/05. Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        Func<int[], int> smallestNumber = numbers =>
+        {
+            int minNum = int.MaxValue;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] < minNum)
+                {
+                    minNum = input[i];
+                }
+            }
+            return minNum;
+        };
+        Console.WriteLine(smallestNumber(input));
+    }
+}
