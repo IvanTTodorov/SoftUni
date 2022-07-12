9e561a2cec55df8f5f88092a25effb8ad655abca
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-12 21:34:17 +0300

    Added a new task

---
+++ Homework/C# Advanced/05. Functional Programming - Exercise/06. ReverseandExclude/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Func<int, int, bool> removingNumbers = (x, n) => x % n == 0; 
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+        int[] result = numbers.Where(x => !removingNumbers(x, n)).Reverse().ToArray();
+
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
