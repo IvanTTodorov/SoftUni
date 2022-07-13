7137413302ef7ffcce3f48298e1392218b1c0915
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-13 19:12:46 +0300

    Added a new task.

---
+++ Homework/C# Advanced/07.Defining Classes - Exercise/02.CreatingConstructors/DefiningClasses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ivan", 54);
+        }
+
+    }
+}
