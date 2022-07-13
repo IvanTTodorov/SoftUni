8537e3f329162fda932cd4b4cb250d96731e0b98
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-13 19:12:11 +0300

    Added a new task.

---
+++ Homework/C# Advanced/07.Defining Classes - Exercise/01. Define a Class Person/DefiningClasses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace DefiningClasses
+{
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Maria";
+            person.Age = 24; 
+        }
+    }
+}
