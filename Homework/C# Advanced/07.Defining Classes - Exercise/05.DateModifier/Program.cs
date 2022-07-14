e507a7477a10891efb3bcdbadb8b034651fd7d1f
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-14 21:50:48 +0300

    Added a new task.

---
+++ Homework/C# Advanced/07.Defining Classes - Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine(); 
+            string dateTwo = Console.ReadLine();
+            Console.WriteLine(DateModifier.Calculate(dateOne , dateTwo));
+
+        }
+    }
+    public static class DateModifier
+    {
+        public static int Calculate (string dateOne, string dateTwo)
+        {
+            DateTime firstDate = DateTime.Parse(dateOne);
+            DateTime secondDate = DateTime.Parse(dateTwo); 
+
+            int calculator = Math.Abs((firstDate - secondDate).Days);
+            return calculator; 
+        }
+    }
+}
