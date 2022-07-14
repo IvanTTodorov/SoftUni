7b681d78e7084ef8c3e5e6225c0660df2a617783
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-14 21:41:54 +0300

    Added a new task.

---
+++ Homework/C# Advanced/07.Defining Classes - Exercise/04.OpinionPoll/DefiningClasses.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int n = int.Parse(Console.ReadLine());
+            List<Person> newPeople = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(' ');
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person, family.People);
+
+            }
+            newPeople = family.OldestPeople(family.People); 
+            foreach (var person in newPeople)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+
+        }
+    }
+}
---
+++ Homework/C# Advanced/07.Defining Classes - Exercise/04.OpinionPoll/Family.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; }
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public void AddMember(Person person, List<Person> people)
+        {
+            people.Add(person);
+        }
+        public List<Person> OldestPeople (List <Person> people)
+        {
+            return people.Where(x => x.Age > 30).OrderBy(person => person.Name).ToList();
+        }
+    }
+}
