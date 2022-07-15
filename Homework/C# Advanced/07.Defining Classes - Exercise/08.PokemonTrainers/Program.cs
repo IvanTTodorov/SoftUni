f3f653a697a1adfeb40b3248acb96eb50b722819
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-16 00:23:07 +0300

    Added a new task

---
+++ Homework/C# Advanced/07.Defining Classes - Exercise/08.PokemonTrainers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Trainers> allTrainers = new List<Trainers>();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] pokemonInfo = input.Split(' ');
+                string trainerName = pokemonInfo[0];
+                string pokemonName = pokemonInfo[1];
+                string element = pokemonInfo[2];
+                int health = int.Parse(pokemonInfo[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, element, health);
+
+                if (!allTrainers.Any(x => x.Name == trainerName))
+                {
+                    Trainers trainer = new Trainers(trainerName, 0);
+                    trainer.Pokemon.Add(pokemon);
+                    allTrainers.Add(trainer);
+
+                }
+                else
+                {
+                    foreach (var trainer in allTrainers)
+                    {
+                        if (trainer.Name == trainerName)
+                        {
+                            trainer.Pokemon.Add(pokemon);
+                        }
+                    }
+                }
+
+
+            }
+            string text = Console.ReadLine();
+            while (text != "End")
+            {
+                foreach (var trainer in allTrainers)
+                {
+                        if (trainer.Pokemon.Any(x => x.Element == text))
+                        {
+                            trainer.NumOfBadgets++;
+                        }
+                        else
+                        {
+                            foreach (var pokemon1 in trainer.Pokemon)
+                            {
+                                pokemon1.Health -= 10;
+                            }
+                            trainer.Pokemon.RemoveAll(x => x.Health <= 0);
+                        }
+                    } 
+
+                text = Console.ReadLine(); 
+            }
+            foreach (var trainer in allTrainers.OrderByDescending(x => x.NumOfBadgets))  
+                {
+                Console.WriteLine($"{trainer.Name} {trainer.NumOfBadgets} {trainer.Pokemon.Count}");
+            }
+            
+        }
+    }
+}
---
+++ Homework/C# Advanced/07.Defining Classes - Exercise/08.PokemonTrainers/Trainers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Trainers
+    {
+        public string Name { get; set; }
+        public int NumOfBadgets { get; set; }
+        public List <Pokemon> Pokemon { get; set; }
+        public Trainers(string name, int numIfBadgets)
+        {
+            Name = name;
+            NumOfBadgets = 0;
+            Pokemon = new List<Pokemon>();
+        
+    }
+    }
+}
