a6264f145cceb8b7bab63f20998bec0927c854cd
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-17 23:00:53 +0300

    Added a new task.

---
+++ Homework/C# Advanced/07. Defining Calsses - Lab/03. Car Constructors/CarConstructors.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace CarManufacturer
+{
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine()); 
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car(); 
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption); 
+        }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) 
+            : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity; 
+            FuelConsumption = fuelConsumption; 
+        }
+    }
+}
