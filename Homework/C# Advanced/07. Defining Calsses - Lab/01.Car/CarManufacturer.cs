5b14ac20c77d0cf272fed076a8aa4a2ea06c38a5
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-17 22:59:40 +0300

    Added a new task.

---
+++ Homework/C# Advanced/07. Defining Calsses - Lab/01.Car/CarManufacturer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace CarManufacturer
+{
+
+   public class StartUp
+    {
+  
+    static void Main(string[] args)
+        {
+            Car cars = new Car();
+            cars.Make = "VW";
+            cars.Model = "MK3";
+            cars.Year = 1992;
+
+            Console.WriteLine($"Make: {cars.Make}");
+            Console.WriteLine($"Model: {cars.Model}");
+            Console.WriteLine($"Year: {cars.Year}");
+        }
+    }
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
