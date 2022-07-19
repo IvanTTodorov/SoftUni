c2995bf2463890a1c82195d8f0b3e14e61b8d292
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-19 21:35:23 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generics - Lab/01. Box of T/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> list;
+        public int Count => this.list.Count;
+
+        public Box()
+        {
+            this.list = new List<T>();
+        }
+        public void Add (T element)
+        {
+            this.list.Add(element);
+        }   
+
+        public T Remove ()
+        {
+            T element = this.list.Last();
+            list.RemoveAt(this.list.Count - 1);
+            return element;
+        } 
+
+
+    }
+}
