f580a2523cf52e2d128c1536ee928610dd795154
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-19 21:36:39 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generics - Lab/03. GenericScale/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        { 
+            EqualityScale<int> equalityScale = new EqualityScale<int>(10,5);
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}
