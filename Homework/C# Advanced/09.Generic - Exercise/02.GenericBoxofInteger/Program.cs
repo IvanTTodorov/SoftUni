64cc95eaa6a14e1bd18c0a3cfca0b04f90649768
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-20 21:25:45 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/02.GenericBoxofInteger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace GenericBoxofInt
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
