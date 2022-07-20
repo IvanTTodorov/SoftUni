050ef502fe139fbf5e4ed035f49e941c7303092f
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-20 21:25:10 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/01.GenericBoxofString/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxofString
+{
+    public class Box
+    {
+        private List<string> boxes;
+
+        public Box()
+        {
+            boxes = new List<string>();
+        }
+        public void Add(string box)
+        {
+            boxes.Add(box);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(); 
+
+          foreach (string box in boxes)
+            {
+                sb.AppendLine($"System.String: { box}");
+            }
+          return sb.ToString().TrimEnd();
+        }
+    }
+}
