93aaab130ea29110ede055540e5947ff4ee6a41c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-21 21:43:33 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/04.GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxofInt
+{
+    public class Box
+    {
+        private List<int> boxes;
+
+        public Box()
+        {
+            boxes = new List<int>();
+        }
+        public void Add(int box)
+        {
+            boxes.Add(box);
+        }
+        public void Swap (int input1, int input2)
+        {
+            int temp = boxes[input1];
+            boxes[input1] = boxes[input2];
+            boxes[input2] = temp;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (int box in boxes)
+            {
+                sb.AppendLine($"{typeof(int)}: {box}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
+
+
---
+++ Homework/C# Advanced/09.Generic - Exercise/04.GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace GenericBoxofInt
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            box.Swap(nums[0], nums[1]); 
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
