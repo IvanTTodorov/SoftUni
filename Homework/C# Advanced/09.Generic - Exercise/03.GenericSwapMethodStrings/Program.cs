6e27ef165242d0b759de4b8faf31e29349e31c7b
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-21 21:39:18 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace GenericBoxofString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            box.Swap(numbers[0], numbers[1]); 
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
