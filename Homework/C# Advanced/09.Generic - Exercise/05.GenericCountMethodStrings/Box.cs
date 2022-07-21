c9e50d4e80ca4f847a49f2bad7d311fb8fd44400
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-21 21:44:28 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/05.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        private List<T> boxes;
+
+        public Box()
+        {
+            boxes = new List<T>();
+        }
+        public void Add(T box)
+        {
+            boxes.Add(box);
+        } 
+         
+        public int ReturnCountOfGreaterElements (T element)
+        {
+            int count = 0;
+            foreach (var item in boxes)
+            {
+                if(item.CompareTo(element) > 0)  
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+   
+       
+    }
+}
---
+++ Homework/C# Advanced/09.Generic - Exercise/05.GenericCountMethodStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace GenericBoxofString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Box<string> box = new Box<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            string elementToBeCompared = Console.ReadLine();
+
+            int result = box.ReturnCountOfGreaterElements(elementToBeCompared);
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
