186f31a2a0cc902284fef4630bb5c5d1ec9720b3
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-22 19:59:33 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/07. Tuple/MyTuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple
+{
+    public class MyTuple <Item1, Item2>
+    {
+        public Item1 LeftItem { get; set; }
+        public Item2 RightItem { get; set; }
+        public MyTuple(Item1 left, Item2 right)
+        {
+            LeftItem = left;
+            RightItem = right;
+        }
+        public string GetItems()
+        {
+            return $"{LeftItem} -> {RightItem}"; 
+        }
+    }
+}
---
+++ Homework/C# Advanced/09.Generic - Exercise/07. Tuple/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] nameTownInput = Console.ReadLine().Split();
+            string firstName = nameTownInput[0];
+            string secondName = nameTownInput[1];
+            string town = nameTownInput[2]; 
+
+            string[] nameAndBeer = Console.ReadLine().Split();
+            string name = nameAndBeer[0];
+            int amountOfBeer = int.Parse(nameAndBeer[1]);
+
+            string[] thirdArr = Console.ReadLine().Split();
+            int integer = int.Parse(thirdArr[0]);
+            double db = double.Parse(thirdArr[1]);
+
+
+            MyTuple<string, string> NameAndTown = new MyTuple<string, string>
+                ($"{firstName} {secondName}", town);
+
+
+            MyTuple<string, int> NameAndBeer = new MyTuple<string, int>
+                (name,amountOfBeer);
+
+            MyTuple<int, double> IntegerAndDouble = new MyTuple<int, double>
+                (integer,db);
+
+            Console.WriteLine(NameAndTown.GetItems());
+            Console.WriteLine(NameAndBeer.GetItems());
+            Console.WriteLine(IntegerAndDouble.GetItems()); 
+
+        }
+    }
+}
