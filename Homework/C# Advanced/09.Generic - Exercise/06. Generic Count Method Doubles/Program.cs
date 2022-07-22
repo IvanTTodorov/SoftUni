8594cb2f6d97b11655f248d036602dcb46983a2b
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-22 19:58:50 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/06. Generic Count Method Doubles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace GenericBoxofString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Box<double> box = new Box<double>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+
+            double elementToBeCompared = double.Parse(Console.ReadLine());
+
+            int result = box.ReturnCountOfGreaterElements(elementToBeCompared);
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
