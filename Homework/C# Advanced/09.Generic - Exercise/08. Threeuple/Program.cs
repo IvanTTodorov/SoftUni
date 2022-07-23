ed0c82ad6569940afe88ca1da93fd5deeba2f6a1
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-23 19:03:42 +0300

    Added a new task.

---
+++ Homework/C# Advanced/09.Generic - Exercise/08. Threeuple/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] firstInput = Console.ReadLine().Split();
+            string firstName = $"{firstInput[0]} {firstInput[1]}";
+            string adress = firstInput[2];
+            string town = String.Join(" ", firstInput.Skip(3));
+            
+
+            string[] secondInput = Console.ReadLine().Split();
+            string name = secondInput[0];
+            int amountOfBeer = int.Parse(secondInput[1]);
+            string drunkOrNot = secondInput[2];
+            bool drunkBeer = true;
+            if (drunkOrNot == "drunk")
+            {
+                drunkBeer = true;
+            }
+            else
+            {
+                drunkBeer = false;
+            }
+
+
+            string[] thirdInput = Console.ReadLine().Split();
+            string nameAccount = thirdInput[0];
+            double amount = double.Parse(thirdInput[1]);
+            string bankName = thirdInput[2];
+
+
+                Threeuple<string, string, string> NameAndTown = new Threeuple<string, string, string>
+                       (firstName,  adress, town);
+                Console.WriteLine(NameAndTown.GetItems());
+
+            Threeuple<string, int, bool> NameAndBeer = new Threeuple<string, int, bool>
+                (name, amountOfBeer, drunkBeer);
+
+            Threeuple<string, double, string> NameAndBankAccount = new Threeuple<string, double, string>
+                (nameAccount, amount, bankName);
+
+            Console.WriteLine(NameAndBeer.GetItems());
+            Console.WriteLine(NameAndBankAccount.GetItems());
+
+        }
+    }
+}
---
+++ Homework/C# Advanced/09.Generic - Exercise/08. Threeuple/Threeuple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threeuple
+{
+    public class Threeuple <Item1,Item2,Item3>
+    {
+        public Item1 Left { get; set; }
+        public Item2 Middle { get; set; }
+        public Item3 Right { get; set; }
+
+        public Threeuple(Item1 left, Item2 middle, Item3 right)
+        {
+            Left = left;
+            Middle = middle;
+            Right = right;
+        }
+
+        public string GetItems()
+        {
+            return $"{Left} -> {Middle} -> {Right}";
+        }
+    }
+}
