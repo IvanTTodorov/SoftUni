286a4a2ffa32299ff54aa3ce5153d227b01bcf5c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-25 18:46:58 +0300

    Added a new task.

---
+++ Homework/C# Advanced/10.Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class ListyIterator<T> 
+    {
+        private List<T> array;
+        private int index;
+
+        public ListyIterator(params T[] data)
+        {
+            array = new List<T>(data);
+            index = 0; 
+        }
+        public bool HasNext()
+        {
+            return index + 1 < array.Count;
+        }
+        public bool Move()
+        {
+           
+            if(HasNext())
+            {
+                index++;
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Print ()
+        {
+            if (array.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(array[index]);
+            }
+        }
+    }
+}
