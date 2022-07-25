ca1b8382be799e17d55193daf6951cc45dd54089
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-25 18:47:30 +0300

    Added a new task.

---
+++ Homework/C# Advanced/10.Iterators and Comparators - Exercise/02. Collection/ListyIterator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> array;
+        private int index;
+
+        public ListyIterator(params T[] data)
+        {
+            array = new List<T>(data);
+            index = 0;
+        }
+
+        public bool HasNext()
+        {
+            return index + 1 < array.Count;
+        }
+        public bool Move()
+        {
+
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Print()
+        {
+            if (array.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(array[index]);
+            }
+        }   
+        public void PrintAll()
+        {
+            Console.WriteLine(String.Join(" ", array));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Homework/C# Advanced/10.Iterators and Comparators - Exercise/02. Collection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split();
+            ListyIterator<string> listy = null;
+
+            while (command[0] != "END")
+            {
+                if (command[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(command.Skip(1).ToArray());
+                }
+                else if (command[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+                else if (command[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (command[0] == "Print")
+                {
+                    listy.Print();
+                } 
+                else if (command[0] == "PrintAll")
+                {
+                    listy.PrintAll();
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+        }
+    }
+}
