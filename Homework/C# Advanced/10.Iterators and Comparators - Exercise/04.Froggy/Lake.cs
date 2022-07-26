5bae61c28704bf2274bc00e0a87841f13ae4ba01
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-26 23:27:55 +0300

    Added a new task.

---
+++ Homework/C# Advanced/10.Iterators and Comparators - Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] lake;
+
+        public Lake(params int[] stones) 
+        {
+            lake = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+
+            for (int i = 0; i < lake.Length; i++)
+            {
+                if( i % 2 == 0)
+                {
+                    yield return lake[i];
+                }
+            }
+            for (int i = lake.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 == 1)
+                {
+                    yield return lake[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator(); 
+        }
+    }
+}
---
+++ Homework/C# Advanced/10.Iterators and Comparators - Exercise/04.Froggy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Lake lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
