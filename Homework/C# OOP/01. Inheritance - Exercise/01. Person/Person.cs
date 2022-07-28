a109a96f9121e196e878c0bbd4451bf0a7b99fb9
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-28 08:18:25 +0300

    Added a new task.

---
+++ Homework/C# OOP/01. Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }  
+            set
+            {
+                name = value; 
+            }
+        } 
+        public int Age
+        {
+            get
+            {
+                return age;
+            } 
+            set
+            { 
+                age = value; 
+            }
+        }
+    
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age; 
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"Name: {Name}, ");
+            stringBuilder.Append($"Age: {Age}");
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
---
+++ Homework/C# OOP/01. Inheritance - Exercise/01. Person/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace Person
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 0)
+            {
+                return;
+            }
+            Person person;
+
+            if (age <= 15)
+            {
+                person = new Child(name, age);
+            }
+            else
+            {
+                person = new Person(name, age);
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
