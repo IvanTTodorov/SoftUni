cac28bff41be2b5996093189b5bb6732211c35b0
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-28 08:16:33 +0300

    Added a new task.

---
+++ Homework/C# OOP/01. Inheritance - Lab/02.MultipleInheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+
+        }
+    }
+}
