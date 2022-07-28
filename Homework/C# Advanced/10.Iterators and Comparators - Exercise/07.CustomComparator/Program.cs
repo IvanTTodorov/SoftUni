98cec2020b2e821bfc0c9238773f7dbb886666e7
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-28 08:11:49 +0300

    Added a new task.

---
+++ Homework/C# Advanced/10.Iterators and Comparators - Exercise/07.CustomComparator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, int, int> func = (num1, num2) =>
+              (num1 % 2 == 0) && (num2 % 2 != 0) ? -1 :
+              (num1 % 2 != 0) && (num2 % 2 == 0) ? 1 :
+              num1.CompareTo(num2);
+
+            Array.Sort<int>(arr, new Comparison<int>(func));
+            Console.WriteLine(String.Join(" ", arr));
+
+        }
+    }
+}
