619e62a9c8d6aefc0e7513d44bb327720bf2d7f8
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-30 22:05:56 +0300

    Added a new task.

---
+++ Homework/C# OOP/01. Inheritance - Lab/04.RandomList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new RandomList();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+            list.Add("5");
+            list.RemoveRandomElement();
+            list.RemoveRandomElement();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(list.RandomString());
+            }
+           
+        }
+    }
+}
---
+++ Homework/C# OOP/01. Inheritance - Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+        public RandomList()
+        {
+            random = new Random();  
+        }
+        public string RandomString()
+        { 
+            int index = random.Next(0,Count);
+            return this[index];
+        } 
+           
+        public void RemoveRandomElement()
+        {
+            int index = random.Next(0, Count);
+            RemoveAt(index);
+        }
+    }
+}
