16c0eed694319333b7d65861cc27cabb5b909d1c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-07-31 12:17:49 +0300

    Added a new task.

---
+++ Homework/C# OOP/01. Inheritance - Lab/05.Stack of Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>() { "ivan", "maria" };
+            
+            StackOfStrings myStack = new StackOfStrings();
+         
+            Console.WriteLine(myStack.IsEmpty()); 
+
+            myStack.AddRandge(list);
+            Console.WriteLine(String.Join(" ", list));
+            Console.WriteLine(String.Join(" ", myStack));
+            Console.WriteLine(myStack.IsEmpty());
+        }
+    }
+}
---
+++ Homework/C# OOP/01. Inheritance - Lab/05.Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    { 
+        public bool IsEmpty()
+        {
+            return Count == 0; 
+        } 
+        public void AddRandge(IEnumerable<string> items)
+        {
+           foreach(string item in items)
+            {
+                Push(item);
+            }
+        }
+    }
+}
+ 
