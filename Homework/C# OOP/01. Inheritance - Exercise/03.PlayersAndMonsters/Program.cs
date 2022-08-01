85129d79fb7a38de0273c5c053ec58b48601837c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-01 09:24:16 +0300

    Added a new task.

---
+++ Homework/C# OOP/01. Inheritance - Exercise/03.PlayersAndMonsters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Elf("ivan", 100);
+            Console.WriteLine(hero);
+        }
+    }
+}
