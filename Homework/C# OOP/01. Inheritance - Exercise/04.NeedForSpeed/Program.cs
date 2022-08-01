d95c762160329e7e83d0f8c4eced3192c1b502e9
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-01 09:24:50 +0300

    Added a new task.

---
+++ Homework/C# OOP/01. Inheritance - Exercise/04.NeedForSpeed/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Car(100, 105.5);
+            Vehicle raceMotocycle = new RaceMotorcycle(100, 105.5);
+            car.Drive(1);
+            raceMotocycle.Drive(1);
+            
+        }
+    }
+}
---
+++ Homework/C# OOP/01. Inheritance - Exercise/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+        public virtual double FuelConsumption { get; set; }
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+            FuelConsumption = DefaultFuelConsumption;
+        } 
+
+        public void Drive (double km)
+        {
+            Fuel -= km * FuelConsumption;
+        }
+    }
+}
