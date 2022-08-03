01135d414a707c236d7494c2013caed2616c70cb
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-03 20:37:05 +0300

    Added a new task.

---
+++ Homework/C# OOP/02. Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width; 
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+           private  set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public string SurfaceArea()
+        {
+            double result = 2 * (Length * Width) + 2 * (Width * Height) + 2 * (Height * Length);
+            return $"Surface Area - {result:f2}";
+        }
+        public string LateralSurfaceArea()
+        {
+            double result = 2 * (Height * Length) + 2 * (Width * Height); 
+            return $"Lateral Surface Area - {result:f2}";
+        }
+        public string Volume()  
+        {
+          double result =   Length * Width * Height;
+            return $"Volume - {result:f2}";
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Homework/C# OOP/02. Encapsulation - Exercise/01.ClassBoxData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine(box.SurfaceArea());
+                Console.WriteLine(box.LateralSurfaceArea());
+                Console.WriteLine(box.Volume());
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
