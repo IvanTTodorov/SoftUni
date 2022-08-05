d32b1e6e7b7939199f284034108406fa3f2341af
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-05 21:53:02 +0300

    Added a new task.

---
+++ Homework/C# OOP/02. Encapsulation - Lab/02. Salary Increase/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public void IncreaseSalary(decimal percantage)
+        {
+            if(Age >= 30)
+            {
+                Salary += Salary * (percantage / 100);
+            } 
+            else
+            {
+                Salary += Salary * (percantage / 200);
+
+            }
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
