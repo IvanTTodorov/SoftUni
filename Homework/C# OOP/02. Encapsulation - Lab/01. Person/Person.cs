048465115cde1c52800f435ee5cc2a6daa3f92d3
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-05 21:52:35 +0300

    Added a new task.

---
+++ Homework/C# OOP/02. Encapsulation - Lab/01. Person/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age) 
+        {
+            FirstName = firstName;   
+            LastName = lastName;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
