d25142afe283f738b475701af13e8c25dfef5489
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-06 20:48:00 +0300

    Added a new task.

---
+++ Homework/C# OOP/02. Encapsulation - Lab/03. ValidationOfData/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary; 
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+           private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                    
+                }
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+           private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                    
+                }
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+           private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                    
+                }
+                age = value;
+            }
+        }
+        public decimal Salary {
+            get
+            {
+                return salary;
+            }
+           private set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percantage)
+        {
+            if (Age >= 30)
+            {
+                Salary += Salary * (percantage / 100);
+            }
+            else
+            {
+                Salary += Salary * (percantage / 200);
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ Homework/C# OOP/02. Encapsulation - Lab/03. ValidationOfData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2])
+                    , decimal.Parse(personInfo[3]));
+
+                people.Add(person);
+            }
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            foreach (var person in people)
+            {
+                person.IncreaseSalary(percentage);
+                Console.WriteLine(person.ToString());
+            }
+
+
+        }
+    }
+}
