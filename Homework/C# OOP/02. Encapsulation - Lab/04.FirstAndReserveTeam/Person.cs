b89584135aeb45a46a5737a0d67af30984ee15b4
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-06 20:48:33 +0300

    Added a new task.

---
+++ Homework/C# OOP/02. Encapsulation - Lab/04.FirstAndReserveTeam/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+            
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+               
+                lastName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                
+                salary = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percantage)
+        {
+            if (Age >= 30)
+            {
+                Salary += Salary * (percantage / 100);
+            }
+            else
+            {
+                Salary += Salary * (percantage / 200);
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ Homework/C# OOP/02. Encapsulation - Lab/04.FirstAndReserveTeam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Collections.Generic;
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                Person person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2])
+                    , decimal.Parse(personInfo[3]));
+
+                people.Add(person);
+            }
+
+            Team team = new Team("Litex");
+            foreach (var person in people)
+            {
+                team.AddPlayer(person);
+            }
+             
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+            
+
+
+        }
+    }
+}
