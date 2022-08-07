4f74c980fcebcc2ffb412c628edbee59c591eb76
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-07 17:36:43 +0300

    Added a new task.

---
+++ Homework/C# OOP/03. Interfaces and Abstaction - Exercise/02.MultipleImplementation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine()); 
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            Citizen person = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(person.Id);
+            Console.WriteLine(person.Birthdate);
+          
+
+        }
+    }
+}
+ 
