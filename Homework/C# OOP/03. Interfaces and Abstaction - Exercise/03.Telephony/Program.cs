63777027f0e696c5b13de1d550adeab787fcd9e3
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-08 19:21:53 +0300

    Added a new task.

---
+++ Homework/C# OOP/03. Interfaces and Abstaction - Exercise/03.Telephony/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Telephony
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SmartPhone smartPhone = new SmartPhone(); 
+            smartPhone.Run();
+           
+                
+            }
+        }
+    }
+
---
+++ Homework/C# OOP/03. Interfaces and Abstaction - Exercise/03.Telephony/SmartPhone.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class SmartPhone : ICallable, IBrowseble
+    {
+        private IList<string> phoneNumbes;
+        private IList<string> sites;
+
+        public SmartPhone()
+        {
+            this.phoneNumbes = new List<string>();
+            this.sites = new List<string>();
+        }
+        public string Browse(string url)
+        {  
+            if(url.Any(x=> Char.IsDigit(x)))
+            {
+                return "Invalid URL!";
+            } 
+            else
+            {
+                return $"Browsing: {url}!";
+            }
+
+   }
+
+        public string Call(string number)
+        {
+            if(!number.All(x=> Char.IsDigit(x)))  
+            {
+               return "Invalid number!";
+            } 
+            if(number.Length == 10)
+            {
+                return $"Calling... {number}";
+            } 
+            else
+            {
+                return $"Dialing... {number}";
+            }
+        }
+
+        public void Run()
+        {
+            phoneNumbes = Console.ReadLine().Split();
+            sites = Console.ReadLine().Split();
+
+            callPhoneNum();
+            browseURL();
+        }
+
+        private void callPhoneNum()
+        {
+            foreach (var item in phoneNumbes)
+            {
+                Console.WriteLine(Call(item));
+            }
+        }
+
+        private void browseURL()
+        {
+            foreach (var item in sites)
+            {
+                Console.WriteLine(Browse(item));
+            }
+        }
+    }
+}
