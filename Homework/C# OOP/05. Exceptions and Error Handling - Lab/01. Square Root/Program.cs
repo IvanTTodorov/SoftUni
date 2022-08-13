9be14634ef0b1d5957b4cfaf4b9dcaa093bedbcb
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-13 18:53:39 +0300

    Added a new task.

---
+++ Homework/C# OOP/05. Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(result(n));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+        } 
+
+        public static double result(double n)  
+        {  
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            } 
+            else
+            {
+                return Math.Sqrt(n);
+            }
+        }
+    }
+}
