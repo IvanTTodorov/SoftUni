6e2c32144b16b3cb95e1955404e75998ea4db345
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-15 13:22:48 +0300

    Added a new task.

---
+++ Homework/C# OOP/05. Exceptions and Error Handling - Lab/04.SumOfIntegers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int total = 0;
+            int count = -1;
+
+            while (count < arr.Length - 1)
+            {
+                try
+                {
+                    count++;
+                    int num = MyParse(arr[count]);
+                    total += num;
+
+                    
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{arr[count]}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{arr[count]}' is out of range!");
+                }  
+             
+
+                Console.WriteLine($"Element '{arr[count]}' processed - current sum: {total}");
+            }
+            Console.WriteLine($"The total sum of all integers is: {total}");
+
+        }
+
+        private static int MyParse(string v)
+        {
+            return int.Parse(v);
+        }
+    }
+}
