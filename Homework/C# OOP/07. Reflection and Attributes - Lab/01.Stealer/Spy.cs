f7b3dbfa3c2cb753793b8b93eb6627271727ffd0
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-16 21:11:18 +0300

    Added a new task.

---
+++ Homework/C# OOP/07. Reflection and Attributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+       public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] classFields = classType.GetFields 
+                (BindingFlags.Public  
+                |BindingFlags.NonPublic 
+                | BindingFlags.Static 
+                | BindingFlags.Instance); 
+             StringBuilder sb = new StringBuilder(); 
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
