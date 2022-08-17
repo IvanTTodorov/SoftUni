706ca2d05309ef8e97bcf9bce724f94e063c9d3b
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-17 18:27:29 +0300

    Added a new task.

---
+++ Homework/C# OOP/07. Reflection and Attributes - Lab/02.HighQualityMistakes/Spy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = typeof(Hacker); 
+            FieldInfo[] allFields = classType.GetFields(BindingFlags.Instance 
+                | BindingFlags.Static | BindingFlags.Public);
+
+            MethodInfo[] allPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] allNonPublicethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder(); 
+
+            foreach (var field in allFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            foreach (MethodInfo method in allPublicMethods)
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+            foreach (MethodInfo method in allNonPublicethods)
+            {
+                sb.AppendLine($"{method.Name} have to be private");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
