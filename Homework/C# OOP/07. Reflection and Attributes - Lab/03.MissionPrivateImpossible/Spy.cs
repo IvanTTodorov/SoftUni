1cd7f455a8910de1e9730b4d0e1ed9695c986497
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-17 18:27:54 +0300

    Added a new task.

---
+++ Homework/C# OOP/07. Reflection and Attributes - Lab/03.MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        
+        public string RevealPrivateMethods(string className)
+        {
+            Type classType = typeof(Hacker); 
+ 
+            MethodInfo[] allNonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"All Private Methods of Class: {className}");
+            sb.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            foreach (MethodInfo method in allNonPublicMethods)
+            {
+                sb.AppendLine(method.Name); 
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
