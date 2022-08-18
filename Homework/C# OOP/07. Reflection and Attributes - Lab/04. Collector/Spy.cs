f41f2e103a01f4de5689ba25f94a7e528a4855f2
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-18 19:40:18 +0300

    Added a new task.

---
+++ Homework/C# OOP/07. Reflection and Attributes - Lab/04. Collector/Spy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        
+        public string RevealPrivateMethods(string className)
+        {
+            Type classType = typeof(Hacker); 
+ 
+            MethodInfo[] allMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic
+                | BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+            
+
+            foreach (MethodInfo method in allMethods.Where(x => x.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}"); 
+            }
+            foreach (MethodInfo method in allMethods.Where(x => x.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
