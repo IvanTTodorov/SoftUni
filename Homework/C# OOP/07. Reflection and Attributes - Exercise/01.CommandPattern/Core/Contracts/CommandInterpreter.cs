ef31d9005c0f662f6a72258e3ac99685e7b53eb6
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-19 21:18:20 +0300

    Added a new task.

---
+++ Homework/C# OOP/07. Reflection and Attributes - Exercise/01.CommandPattern/Core/Contracts/CommandInterpreter.cs
@@ -0,0 +1,30 @@
+﻿using CommandPattern.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core.Contracts
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandInfo = args.Split();
+            string commandName = commandInfo[0] + "Command";
+            string[] parameters = commandInfo.Skip(1).ToArray();
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().Where(x => x.Name == commandName).FirstOrDefault();
+            if (type == null)
+            {
+                throw new InvalidOperationException("Invalid command");
+            }
+
+            ICommand command = (ICommand)Activator.CreateInstance(type);
+            string result = command.Execute(parameters);
+
+            return result;
+        }
+    }
+}
