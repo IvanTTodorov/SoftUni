07b1f32550d253f3fb5e69a04a68f85cf2e94a61
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-19 21:17:16 +0300

    Added a new task.

---
+++ Homework/C# OOP/07. Reflection and Attributes - Lab/06.CodeTracker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Ivan")]
+    public class StartUp
+    {
+        [Author("George")]
+
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor("Ivan");
+
+        }
+
+    }
+}
---
+++ Homework/C# OOP/07. Reflection and Attributes - Lab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor(string name)
+        {
+            Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                foreach (MethodInfo method in type.GetMethods
+                    (BindingFlags.Instance  
+                    | BindingFlags.Public
+                    | BindingFlags.NonPublic  
+                    | BindingFlags.Static))
+                {
+                    AuthorAttribute attr = method.GetCustomAttribute<AuthorAttribute>(); 
+                    if(attr != null && attr.Name == name)
+                    {
+                        Console.WriteLine(method.Name);
+                    }
+                }
+                
+            }
+
+        }
+    }
+}
