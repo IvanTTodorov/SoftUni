3b09c10ea3fa1c18e79c5a98871d8c6fda08a6e3
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-20 17:40:44 +0300

    Added a new task.

---
+++ Homework/C# OOP/08. Unit Testing - Lab/01.TestAxe/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Axe axe = new Axe(10, 0);
+        Dummy dummy = new Dummy(10, 10);
+
+        axe.Attack(dummy);
+        Console.WriteLine(axe.DurabilityPoints);
+    }
+}
