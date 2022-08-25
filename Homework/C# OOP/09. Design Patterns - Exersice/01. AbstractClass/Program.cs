4bfa58e32097cbe6bca4ce0e6c0847fbab8574ef
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-25 21:18:40 +0300

    Added a new task.

---
+++ Homework/C# OOP/09. Design Patterns - Exersice/01. AbstractClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SandwichMenu sandwichMenu = new SandwichMenu();
+
+            sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+            sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut Butter, Jelly");
+            sandwichMenu["Turkey"] = 
+                new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+
+
+            sandwichMenu["LoadedBLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato, Onion, Olives");
+
+
+            Sandwich sandwich1 = sandwichMenu["BLT"].Clone() as Sandwich;
+            Sandwich sandwich2 = sandwichMenu["Turkey"].Clone() as Sandwich;
+
+        }
+    }
+}
---
+++ Homework/C# OOP/09. Design Patterns - Exersice/01. AbstractClass/Sandwich Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class SandwichMenu
+    {
+
+        private readonly Dictionary<string, SandwichPrototype> sandwiches;
+
+        public SandwichMenu()
+        {
+            sandwiches = new Dictionary<string, SandwichPrototype>(); 
+        }
+
+        public SandwichPrototype this[string name]
+        {
+            get
+            {
+                return this.sandwiches[name]; 
+            }
+            set
+            {
+                sandwiches.Add(name, value);
+            }
+        }
+    }
+}
