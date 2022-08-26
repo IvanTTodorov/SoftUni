3890a639db62090fa090767228ce43d47bba9f83
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-26 21:54:52 +0300

    Added a new task.

---
+++ Homework/C# OOP/09. Design Patterns - Exersice/02.Composite/CompositeGift.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private readonly ICollection<GiftBase> gifts; 
+
+        public CompositeGift(string name, int price) 
+            : base(name, price)
+        {
+            gifts = new HashSet<GiftBase>();    
+        }
+
+        public void Add(GiftBase gift)
+        {
+            gifts.Add(gift); 
+        }
+        public void Remove(GiftBase gift)
+        {
+            gifts.Remove(gift);
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            int total = 0;
+
+            Console.WriteLine($"{name} contains the following pieces:");
+
+            foreach (var gift in gifts)
+            {
+                total += gift.CalculateTotalPrice();
+            }
+            return total;
+        }
+
+    }
+}
---
+++ Homework/C# OOP/09. Design Patterns - Exersice/02.Composite/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Composite
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var phone = new SingleGift("IPhone", 1000);
+            phone.CalculateTotalPrice();
+
+            var rootBox = new CompositeGift("RootBox", 0);
+            var truck = new SingleGift("Truck", 9);
+            var car = new SingleGift("Car", 9);
+
+            rootBox.Add(truck);
+            rootBox.Add(car);
+
+            Console.WriteLine($"Total price of this composite present is: " +
+                $"{rootBox.CalculateTotalPrice()}");
+        }
+    }
+}
