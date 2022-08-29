7d1202a544e1fb82fe9f4b7c6ede204dfdd4d142
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-29 21:35:03 +0300

    Added a new task.

---
+++ Homework/Data Structures Fundamentals C#/01. Linear Data Structures - Exercise/01.FasterQueue/IAbstractQueue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FasterQueue
+{
+    public interface IAbstractQueue<T> : IEnumerable<T>
+    {
+        int Count { get; }
+        void Enqueue(T item);
+        T Dequeue();
+        T Peek();
+        bool Contains(T item); 
+    }
+}
