8ff39068988aa29e9e2c3adc90af352d3b2d4439
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-30 19:16:01 +0300

    Added a new task.

---
+++ Homework/Data Structures Fundamentals C#/06.Heaps BST- Lab/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,107 @@
+﻿namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T element, IAbstractBinaryTree<T> left, IAbstractBinaryTree<T> right)
+        {
+            this.Value = element; 
+            this.LeftChild = left;
+            this.RightChild = right;
+        }
+
+        public T Value { get; private set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            var sb = new StringBuilder();
+            this.PreOrferDfs(sb, indent, this);
+            return sb.ToString();
+        }   
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.ForEachInOrder(action);
+            } 
+
+            action(this.Value); 
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.ForEachInOrder(action);
+            }
+        }
+
+        public List<IAbstractBinaryTree<T>> InOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.InOrder());
+            } 
+
+            result.Add(this); 
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.InOrder());
+            }
+           
+            return result;
+        }
+
+        public List<IAbstractBinaryTree<T>> PostOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>(); 
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PostOrder());
+            }
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PostOrder());
+            }
+            result.Add(this);
+            return result;
+        }
+
+        public List<IAbstractBinaryTree<T>> PreOrder()
+        {
+            var result = new List<IAbstractBinaryTree<T>>();
+            result.Add(this);
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PreOrder());
+            } 
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PreOrder());
+
+            }
+            return result; 
+        }
+        private void PreOrferDfs(StringBuilder sb, int indent, IAbstractBinaryTree<T> binaryTree)
+        {
+            sb.Append(new string(' ', indent)).AppendLine(binaryTree.Value.ToString());
+            if (binaryTree.LeftChild != null)
+            {
+                this.PreOrferDfs(sb, indent + 2, binaryTree.LeftChild);
+            }
+            if (binaryTree.RightChild != null)
+            {
+                this.PreOrferDfs(sb, indent + 2, binaryTree.RightChild);
+            }
+        }
+    }
+}
