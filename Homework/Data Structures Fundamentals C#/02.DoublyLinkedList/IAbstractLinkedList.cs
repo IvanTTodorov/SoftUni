98ca35067eee56c16e3b6359cd71e572f6a0aed1
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-30 19:01:59 +0300

    Added a new task.

---
+++ Homework/Data Structures Fundamentals C#/02.DoublyLinkedList/IAbstractLinkedList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    public interface IAbstractLinkedList<T> :IEnumerable<T>
+    {
+        int Count { get;  }
+        void AddFirst(T item);
+        T RemoveFirst();
+        T GetFirst(); 
+        void AddLast(T item);
+        T RemoveLast();
+        T GetLast(); 
+    }
+}
