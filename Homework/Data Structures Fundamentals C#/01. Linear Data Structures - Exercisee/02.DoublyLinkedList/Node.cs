9e98bf833b1455079e2ee196359084b4d9aaacc3
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-08-30 18:59:33 +0300

    Added a new task.

---
+++ Homework/Data Structures Fundamentals C#/01. Linear Data Structures - Exercisee/02.DoublyLinkedList/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    public class Node<T>
+    {
+
+        public Node(T item, Node<T> next, Node<T> previous) 
+        {
+            Item = item; 
+            Next = next;
+            Previous = previous; 
+        }
+        public Node()
+        {
+
+        }
+
+        public T Item { get; set; }
+        public Node<T> Next { get; set; }
+        public Node <T> Previous { get; set; }
+
+    }
+}
---
+++ Homework/Data Structures Fundamentals C#/01. Linear Data Structures - Exercisee/02.DoublyLinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var doubledLinkedList = new DoubledLinkedList<string>();
+            doubledLinkedList.AddFirst("Maria");
+            doubledLinkedList.AddLast("Ivan");
+            doubledLinkedList.RemoveFirst(); 
+
+            foreach (var item in doubledLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
