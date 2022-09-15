0a78f2b8ed4c65780f9e640faafeecf3c403a122
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-09-15 23:26:19 +0300

    Added a new task.

---
+++ Homework/JavaScript Advanced/02. Arrays and Nested Arrays - Lab/01.EvenPositionElement.js
@@ -0,0 +1,12 @@
+function solve(input) {
+    let result = ''; 
+
+    for(let i = 0; i < input.length; i++) {
+        if (i % 2 == 0) {
+            result += input[i]; 
+            result += ' '; 
+        }
+    }
+    console.log(result);
+}
+solve(['20', '30', '40', '50', '60']);
