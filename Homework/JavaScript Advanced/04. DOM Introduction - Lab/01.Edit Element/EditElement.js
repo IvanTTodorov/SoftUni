dcad779189b0959c5e855c6c59df5bfa3cf853a9
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-09-29 21:05:39 +0300

    Added a new task.

---
+++ Homework/JavaScript Advanced/04. DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -0,0 +1,8 @@
+
+    function editElement(element, match, replacer){
+    
+        while (element.textContent.includes(match)) {
+        element.textContent = element.textContent.replace(match, replacer); 
+        }
+    };
+    
