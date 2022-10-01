e1b3813e28b9c034766f99695f53cdd764c6102d
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-10-01 09:50:49 +0300

    Added a new task.

---
+++ Homework/JavaScript Advanced/04. DOM Introduction - Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+ let firstText = document.getElementById('text');  
+ firstText.style.display = 'inline';  
+
+ let secondText = document.getElementById('more');  
+ secondText.style.display = 'none';
+
+}
