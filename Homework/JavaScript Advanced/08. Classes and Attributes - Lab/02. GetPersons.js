ba56a1b586652def47d694b326ed60ad65e0467c
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-10-18 22:15:27 +0300

    Added a new task.

---
+++ Homework/JavaScript Advanced/08. Classes and Attributes - Lab/02. GetPersons.js
@@ -0,0 +1,27 @@
+function getPersons() {
+class Person {
+    constructor (firstName, lastName, age, email) {
+        this.firstName = firstName; 
+        this.lastName = lastName; 
+        this.age = age; 
+        this.email = email; 
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+    let arr = []; 
+    const person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    const person2 = new Person('SoftUni');
+    const person3 = new Person('Stephan', 'Johnson', 25);
+    const person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    arr.push(person1, person2, person3, person4);
+    
+    return arr; 
+
+}
+console.log(getPersons()); 
+
