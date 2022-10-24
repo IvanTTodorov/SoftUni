b3728b55483403918532d08bbe5fe148c49e623e
Ivan Todorov <ivantoshkovtodorov9888@abv.bg>
2022-10-24 23:02:07 +0300

    Added a new task.

---
+++ Homework/JavaScript Advanced/09. Prototypes and Inheritance - Exercise/02. String Extension.js
@@ -0,0 +1,62 @@
+(function solve() {
+    String.prototype.ensureStart = function (str) {
+
+        if (!this.startsWith(str)) {
+            return str.concat(this);
+        }
+        return this.toString();
+    }
+
+    String.prototype.ensureEnd = function (str) {
+        if (!this.endsWith(str)) {
+            return this + str
+        }
+        return this.toString();
+    }
+
+    String.prototype.isEmpty = function () {
+        return this.length === 0 ? true : false
+
+    }
+
+    String.prototype.truncate = function (n) {
+        if (n < 3) {
+            return '.'.repeat(n);
+        }
+
+        if (this.toString().length <= n) {
+            return this.toString();
+        }
+        else {
+            let lastIndex = this.toString().substring(0, n-2).lastIndexOf(' '); 
+            if(lastIndex !== -1) { 
+                return this.toString().substring(0,lastIndex)+ '...';
+            } else { 
+                return this.toString().substring(0,n-3)+'...';
+            }
+            
+
+        }
+    }
+ 
+    String.format = function(str, ...params) { 
+        for (let i = 0; i <params.length; i++) { 
+            str = str.replace(`{${i}}`, params[i]);
+        } 
+        return str.toString();
+    }
+
+})
+    ();
+
+
+    let str = 'Hello my string';
+
+    str = String.format('The {0} {1} fox',
+      'quick', 'brown'); 
+      console.log(str);
+    str = String.format('jumps {0} {1}',
+      'dog'); 
+      console.log(str);
+    
+
